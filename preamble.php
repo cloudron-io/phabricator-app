a46b56d13d267a4e54d349c89cb8e8afa004e92a
Girish Ramakrishnan <girish@cloudron.io>
2016-07-11 16:11:29 -0700

    HTTP_X_FORWARDED_FOR is not getting set for some reason

--- preamble.php
+++ preamble.php
@@ -1,6 +1,6 @@
 <?php
 
-$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+// TODO: investigate why this doens't work
+// $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $_SERVER['HTTPS'] = true;
-
 
